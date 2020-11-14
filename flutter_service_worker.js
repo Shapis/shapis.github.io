d6065c30d3c6f24619b0e77f34cb64f843a31119
Henrique Silva <hpsilva+git@pm.me>
2020-11-14 18:40:40 -0300

    Add support for slider background images

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,19 +5,25 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "cc514a9bad7f19428e608a850d63db1b",
-"/": "cc514a9bad7f19428e608a850d63db1b",
+"index.html": "e0ec6e15b983fdc736a1fdedb375de66",
+"/": "e0ec6e15b983fdc736a1fdedb375de66",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "assets/images/news.png": "d1b00a523b9632ef42115837f097d48b",
 "assets/images/beauty.png": "6858171c439bae5210b0bdf06b9aa3ec",
 "assets/images/accessories.png": "6acb8b37851b85d41103086c7ea9e05c",
 "assets/images/shoes.png": "56eebc627191701d4cb23736f30be824",
 "assets/images/books.png": "c33e85794f87999f42815a011bbaffdc",
+"assets/images/gradient2.jpeg": "b214212a37e40c7a501b99be899ea3f9",
+"assets/images/madeira2.jpeg": "a632bc7a84dcbc6d9c676b9e6bc7cf2f",
 "assets/images/cart.png": "5acd2b61bdddd4064a96f006db2105d7",
+"assets/images/horizontalgradient1.png": "eb6238ec80f1bca817614024ddd06fff",
 "assets/images/basket.png": "7786cc7eacaa8beafda747ed85ee01e7",
 "assets/images/ofertas.png": "c910e32992987204c84dd57b4231140a",
+"assets/images/rainbowgradient.jpeg": "910f598fedb204f6a45ef762d9ccf20b",
+"assets/images/madeira1.jpeg": "909152e0690923d637c8ee61893017f7",
 "assets/images/ofertas-export.png": "38d58d1d6d823c814b3d7e3142bc4c70",
 "assets/images/ball.png": "d3d670bb04d460c4c25039ebe999d8e3",
+"assets/images/gradient1.jpeg": "6cc239e53262cfa694faa0c7a79d8074",
 "assets/images/hanger.png": "6577fc6154b133ffb9b8fbc9f571b5c5",
 "assets/images/rainbow-gradient.png": "f1a91929effa73a540a09f44c28087d6",
 "assets/FontManifest.json": "c632621213f63a6e0fc9af3ec8b67758",
@@ -28,11 +34,11 @@ const RESOURCES = {
 "assets/fonts/Pacifico-Regular.ttf": "9b94499ccea3bd82b24cb210733c4b5e",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
 "assets/fonts/SourceSansPro-Regular.ttf": "c1678b46f7dd3f50ceac94ed4e0ad01a",
-"assets/AssetManifest.json": "f234886d0ab38e20379bb53dbbb4844c",
+"assets/AssetManifest.json": "7855640d0e43da626dbcadfeea597679",
 "assets/NOTICES": "e2254203461d6ac08e3d3ae837ef17f9",
 "version.json": "dc87bda2c1541d4f6f302404f564f736",
 "manifest.json": "4d62ef1c5a4506a032e4fc82f5bcd3b3",
-"main.dart.js": "80ef3ab33c8208ef2af60792014abfda"
+"main.dart.js": "ea2564861e66b6e5ab90f8afaff08fbd"
 };
 
 // The application shell files that are downloaded before a service worker can
